e76dd42d01c439ae324fabec53b34d25d81528a6
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-12 17:02:58 +0800

    Components: Add Primary Button

---
+++ lib/components/primary_btn.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/color_scheme.dart';
+
+class PRKPrimaryBtn extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const PRKPrimaryBtn({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: blueColor,
+          foregroundColor: whiteColor,
+          padding: EdgeInsets.symmetric(vertical: 12.h),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12.r,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
