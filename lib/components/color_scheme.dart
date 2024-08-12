7d17f2c80181434d93e31f57f1735370a2a13ca0
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-12 17:02:30 +0800

    Components: Add Color Theme

---
+++ lib/components/color_scheme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const blackColor = Color.fromRGBO(27, 27, 27, 1);
+const borderBlack = Color.fromRGBO(27, 27, 27, 10);
+const blueColor = Color.fromRGBO(45, 49, 250, 1);
+const bgColor =  Color.fromRGBO(246, 250, 255, 1);
+const whiteColor =  Color.fromRGBO(255, 255, 255, 1);
