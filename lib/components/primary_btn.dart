803d3cc620069abafb383a728389e929f43a5b3c
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-13 17:52:16 +0800

    Components: Update Primary Button

--- lib/components/primary_btn.dart
+++ lib/components/primary_btn.dart
@@ -16,7 +16,7 @@ class PRKPrimaryBtn extends StatelessWidget {
         style: TextButton.styleFrom(
           backgroundColor: blueColor,
           foregroundColor: whiteColor,
-          padding: EdgeInsets.symmetric(vertical: 12.h),
+          padding: EdgeInsets.symmetric(vertical: 16.h),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
