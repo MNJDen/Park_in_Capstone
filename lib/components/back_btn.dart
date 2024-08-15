3e5b2eee4d238483f61537e035b9561029ad1279
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-15 15:22:05 +0800

    Components: Add Back Button

---
+++ lib/components/back_btn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:park_in/components/color_scheme.dart';
+
+class PRKBackBtn extends StatelessWidget {
+  const PRKBackBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      color: blackColor,
+      enableFeedback: true,
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+    );
+  }
+}
