aad00c00b03f7fcbf84202a59c349f43d6ddc966
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-15 15:22:34 +0800

    Components: Update Color Theme

--- lib/components/color_scheme.dart
+++ lib/components/color_scheme.dart
@@ -5,3 +5,4 @@ const borderBlack = Color.fromRGBO(27, 27, 27, 10);
 const blueColor = Color.fromRGBO(45, 49, 250, 1);
 const bgColor =  Color.fromRGBO(246, 250, 255, 1);
 const whiteColor =  Color.fromRGBO(255, 255, 255, 1);
+const yellowColor =  Color.fromRGBO(255, 219, 35, 1);
