2895c3efa64a631f04d54bdea6c6f2bcc7d15ba1
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-15 15:26:24 +0800

    Screens: Add Sign Up Student 4

---
+++ lib/screens/sign up/sign up student/sign_up_student4.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/color_scheme.dart';
+import 'package:park_in/components/form_field.dart';
+
+class SignUpStudentScreen4 extends StatefulWidget {
+  const SignUpStudentScreen4({super.key});
+
+  @override
+  State<SignUpStudentScreen4> createState() => _SignUpStudentScreen4State();
+}
+
+class _SignUpStudentScreen4State extends State<SignUpStudentScreen4> {
+  final TextEditingController _passwordCtrl = TextEditingController();
+  final TextEditingController _confirmPasswordCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 28.h,
+              ),
+              Text(
+                "What’s your password?",
+                style: TextStyle(
+                  color: blueColor,
+                  fontSize: 24.r,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 4.h,
+              ),
+              Text(
+                "Of course we want your account to be secured, so make sure that your password is strong and a secret.",
+                style: TextStyle(
+                  color: blackColor,
+                  fontSize: 12.r,
+                ),
+              ),
+              SizedBox(
+                height: 32.h,
+              ),
+              PRKFormField(
+                prefixIcon: Icons.password_rounded,
+                labelText: "Password",
+                suffixIcon: Icons.visibility_off_rounded,
+                controller: _passwordCtrl,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 12.h,
+              ),
+              PRKFormField(
+                prefixIcon: Icons.password_rounded,
+                labelText: "Confirm Password",
+                suffixIcon: Icons.visibility_off_rounded,
+                controller: _confirmPasswordCtrl,
+                obscureText: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
