401d0e4adce00813a0ba506cb8435a86a21678af
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-16 14:36:02 +0800

    Screens: Add Sign Up Employee 6

---
+++ lib/screens/sign up/sign up employee/sign_up_employee6.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/color_scheme.dart';
+
+class SignUpEmployeeScreen6 extends StatefulWidget {
+  const SignUpEmployeeScreen6({super.key});
+
+  @override
+  State<SignUpEmployeeScreen6> createState() => _SignUpEmployeeScreen6State();
+}
+
+class _SignUpEmployeeScreen6State extends State<SignUpEmployeeScreen6> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 28.h,
+              ),
+              Text(
+                "What do you look like?",
+                style: TextStyle(
+                  color: blueColor,
+                  fontSize: 24.r,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 4.h,
+              ),
+              Text(
+                "Upload an image with that charming face of yours.",
+                style: TextStyle(
+                  color: blackColor,
+                  fontSize: 12.r,
+                ),
+              ),
+              SizedBox(
+                height: 32.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 164.h,
+                    width: 154.h,
+                    child: Card(
+                      elevation: 15,
+                      shadowColor: Color.fromRGBO(27, 27, 27, 0.2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      color: whiteColor,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(12.w, 16.h, 12.w, 16.h),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton.filled(
+                              iconSize: 20.r,
+                              onPressed: () {},
+                              style: const ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(blueColor),
+                              ),
+                              icon: const Icon(
+                                Icons.add_rounded,
+                                color: whiteColor,
+                              ),
+                            ),
+                            Text(
+                              "Upload a photo",
+                              style: TextStyle(
+                                fontSize: 12.r,
+                                color: blackColor,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
