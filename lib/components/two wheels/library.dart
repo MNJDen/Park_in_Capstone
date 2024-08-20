57656b7730defe7693eef7621c1d71236cdc5deb
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-20 17:03:16 +0800

    Components: Add Two Wheels Student Library Card

---
+++ lib/components/two wheels/library.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/color_scheme.dart';
+
+class PRKLibrary2W extends StatefulWidget {
+  final String parkingArea;
+  final String availableSpace;
+  final String image;
+  final Color? dotColor;
+
+  const PRKLibrary2W({
+    super.key,
+    required this.parkingArea,
+    required this.availableSpace,
+    required this.image,
+    this.dotColor,
+  });
+
+  @override
+  State<PRKLibrary2W> createState() => _PRKLibrary2WState();
+}
+
+class _PRKLibrary2WState extends State<PRKLibrary2W>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _colorAnimation = ColorTween(
+      begin: whiteColor,
+      end: const Color.fromARGB(255, 209, 210, 253),
+    ).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!_animationController.isAnimating) {
+          _animationController.forward(from: 0);
+          Timer(Duration(milliseconds: 350), () {
+            _animationController.reverse();
+          });
+        }
+      },
+      child: AnimatedBuilder(
+        animation: _colorAnimation,
+        builder: (context, child) {
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            height: 140.h,
+            child: Container(
+              decoration: BoxDecoration(
+                color: _colorAnimation.value,
+                borderRadius: BorderRadius.circular(
+                  10,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 38,
+                      left: 21,
+                      child: Text(
+                        widget.availableSpace,
+                        style: TextStyle(
+                          fontSize: 48.r,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: -46,
+                      right: -26,
+                      child: Transform.flip(
+                        flipX: true,
+                        child: Image.asset(
+                          widget.image,
+                          height: 284.h,
+                          width: 234.w,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Container(
+                        width: 7.w,
+                        height: 7.w,
+                        decoration: BoxDecoration(
+                          color: widget.dotColor,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: 10,
+                      child: Text(
+                        widget.parkingArea,
+                        style: TextStyle(
+                          fontSize: 12.r,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
