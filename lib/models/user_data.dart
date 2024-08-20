74282778ccac1848a5e9c7056ee3f7cd8fad27e6
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-08-20 21:15:02 +0800

    Models: Add User Data

---
+++ lib/models/user_data.dart
@@ -0,0 +1,14 @@
+class UserData {
+  String? name;
+  String? userNumber;
+  String? phoneNumber;
+  List<String>? stickerNumber;
+  List<String>? plateNumber;
+
+  UserData(
+      {this.name,
+      this.userNumber,
+      this.phoneNumber,
+      this.stickerNumber,
+      this.plateNumber});
+}
