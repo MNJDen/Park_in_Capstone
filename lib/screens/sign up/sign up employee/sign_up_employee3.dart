62eabd0d75313d1cff612502c5f386c9a7136a33
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-21 21:46:22 +0800

    Screens: Update Sign Up Employee 3

--- lib/screens/sign up/sign up employee/sign_up_employee3.dart
+++ lib/screens/sign up/sign up employee/sign_up_employee3.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:park_in/components/color_scheme.dart';
+import 'package:park_in/components/search_field.dart';
 import 'package:searchfield/searchfield.dart';
 
 class SignUpEmployeeScreen3 extends StatefulWidget {
@@ -12,28 +13,6 @@ class SignUpEmployeeScreen3 extends StatefulWidget {
 
 class _SignUpEmployeeScreen3State extends State<SignUpEmployeeScreen3> {
   final TextEditingController _searchController = TextEditingController();
-
-  bool _isFocused = false;
-
-  late FocusNode _focusNode = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(_onFocusChange);
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  void _onFocusChange() {
-    setState(() {
-      _isFocused = _focusNode.hasFocus;
-    });
-  }
 
   void _onSuggestionTap(String suggestion) {
     setState(() {});
@@ -74,75 +53,24 @@ class _SignUpEmployeeScreen3State extends State<SignUpEmployeeScreen3> {
               SizedBox(
                 height: 32.h,
               ),
-              Material(
-                child: SearchField(
-                  focusNode: _focusNode,
-                  searchInputDecoration: InputDecoration(
-                    filled: true,
-                    fillColor: whiteColor,
-                    prefixIcon: Icon(
-                      size: 20,
-                      Icons.apartment_rounded,
-                      color: _isFocused ? blueColor : blackColor,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        width: 1.w,
-                        color: blueColor,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        width: 0.5.w,
-                        color: borderBlack,
-                      ),
-                    ),
-                    labelText: "Search Department",
-                    labelStyle: TextStyle(
-                      fontSize: 12.r,
-                      color: _isFocused ? blueColor : blackColor,
-                    ),
-                  ),
-                  suggestionStyle: TextStyle(
-                    fontSize: 12.r,
-                    color: blackColor,
-                  ),
-                  suggestionsDecoration: SuggestionDecoration(
-                      elevation: 15,
-                      color: whiteColor,
-                      selectionColor: Color.fromRGBO(45, 49, 250, 0.2),
-                      borderRadius: BorderRadius.circular(10)),
-                  searchStyle: TextStyle(
-                    fontSize: 12.r,
-                    color: blackColor,
-                  ),
-                  scrollbarDecoration: ScrollbarDecoration(
-                    thumbColor: blueColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  offset: Offset(0, 55),
-                  controller: _searchController,
-                  suggestions: [
-                    SearchFieldListItem('Department 1'),
-                    SearchFieldListItem('Department 2'),
-                    SearchFieldListItem('Department 3'),
-                    SearchFieldListItem('Department 4'),
-                    SearchFieldListItem('Department 5'),
-                    SearchFieldListItem('Department 6'),
-                    SearchFieldListItem('Department 7'),
-                    SearchFieldListItem('Department 8'),
-                    SearchFieldListItem('Department 9'),
-                  ],
-                  maxSuggestionsInViewPort: 6,
-                  itemHeight: 40,
-                  onTap: () {
-                    _onSuggestionTap(_searchController.text);
-                  },
-                ),
+              PRKSearchField(
+                prefixIcon: Icons.apartment_rounded,
+                labelText: "Search Department",
+                searchFieldListItems: [
+                  SearchFieldListItem('Department 1'),
+                  SearchFieldListItem('Department 2'),
+                  SearchFieldListItem('Department 3'),
+                  SearchFieldListItem('Department 4'),
+                  SearchFieldListItem('Department 5'),
+                  SearchFieldListItem('Department 6'),
+                  SearchFieldListItem('Department 7'),
+                  SearchFieldListItem('Department 8'),
+                  SearchFieldListItem('Department 9'),
+                ],
+                controller: _searchController,
+                onTap: (text) {
+                  _onSuggestionTap(text);
+                },
               ),
             ],
           ),
