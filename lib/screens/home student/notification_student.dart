caebf1838ddee80536e1c241276f2eeffd506740
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-21 21:45:54 +0800

    Screen: Update Notification Student

--- lib/screens/home student/notification_student.dart
+++ lib/screens/home student/notification_student.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:park_in/components/color_scheme.dart';
-import 'package:park_in/components/notification_card.dart';
+import 'package:park_in/components/notification_announcement_card.dart';
+import 'package:park_in/components/notification_parking_violation_card.dart';
 
 class NotificationStudentScreen extends StatefulWidget {
   const NotificationStudentScreen({super.key});
@@ -71,46 +72,28 @@ class _NotificationStudentScreenState extends State<NotificationStudentScreen> {
               SizedBox(
                 height: 20.h,
               ),
-              PRKNotificationCard(
-                title: "Parking Violation!",
-                date: "06/27/24",
-                description:
-                    "You just got cited a ticket. Check it on your record and settle it asap.",
-                icon: Icons.priority_high_rounded,
-                iconColor: parkingRedColor,
-              ),
+              PRKParkingViolationNotificationCard(date: "06/27/24"),
               SizedBox(
                 height: 12.h,
               ),
-              PRKNotificationCard(
-                title: "Parking Violation!",
-                date: "06/26/24",
-                description:
-                    "You just got cited a ticket. Check it on your record and settle it asap.",
-                icon: Icons.priority_high_rounded,
-                iconColor: parkingRedColor,
-              ),
+              PRKParkingViolationNotificationCard(date: "06/26/24"),
               SizedBox(
                 height: 12.h,
               ),
-              PRKNotificationCard(
+              PRKAnnouncementNotificationCard(
                 title: "Limitation on Parking",
                 date: "06/22/24",
                 description:
                     "No parking on Alingal A, Alingal B, and Covered Court due to graduation rites.",
-                icon: Icons.campaign_rounded,
-                iconColor: parkingYellowColor,
               ),
               SizedBox(
                 height: 12.h,
               ),
-              PRKNotificationCard(
+              PRKAnnouncementNotificationCard(
                 title: "Sticker Renewal",
                 date: "06/22/24",
                 description:
                     "After this intersession, gate pass stickers should be renewed.",
-                icon: Icons.campaign_rounded,
-                iconColor: parkingYellowColor,
               ),
             ],
           ),
