95d30e2437dcc3cfded276e0c9656694ef4f73c7
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-08-22 17:28:53 +0800

    Screens: Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,59 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/bottom_nav_bar_employee.dart';
+import 'package:park_in/components/bottom_nav_bar_student.dart';
 import 'package:park_in/components/color_scheme.dart';
 import 'package:park_in/firebase_options.dart';
 import 'package:park_in/providers/user_data_provider.dart';
 import 'package:park_in/screens/sign%20in/sign_in_student_employee.dart';
-import 'package:park_in/services/auth/Auth_Gate.dart';
+import 'package:park_in/screens/test.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       name: "Park-in", options: DefaultFirebaseOptions.currentPlatform);
+
+  // Check if the user is logged in and get their user type
+  final prefs = await SharedPreferences.getInstance();
+  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  final userType = prefs.getString('userType');
+
+  // Determine the initial screen based on user type
+  Widget initialScreen = const SignInScreen();
+
+  if (isLoggedIn && userType != null) {
+    switch (userType) {
+      case 'Admin':
+        initialScreen = const testScreen();
+        break;
+      case 'Student':
+        initialScreen = const BottomNavBarStudent();
+        break;
+      case 'Employee':
+        initialScreen = const BottomNavBarEmployee();
+        break;
+      default:
+        initialScreen = const SignInScreen();
+    }
+  } else {
+    initialScreen = const SignInScreen();
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserDataProvider(),
-      child: MyApp(),
+      child: MyApp(initialScreen: initialScreen),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget initialScreen;
+
+  const MyApp({required this.initialScreen, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +70,7 @@ class MyApp extends StatelessWidget {
           ).copyWith(
             colorScheme: ThemeData().colorScheme.copyWith(primary: blueColor),
           ),
-          home: AuthGate(),
+          home: initialScreen,
         );
       },
     );
