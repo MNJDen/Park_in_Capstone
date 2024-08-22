f1133ed9e9e0beabf72feddb4b7722a63ae0d4a4
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-22 22:41:10 +0800

    Screens: Update Home Employee 1

--- lib/screens/home employee/home_employee1.dart
+++ lib/screens/home employee/home_employee1.dart
@@ -1,10 +1,20 @@
 import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:navbar_router/navbar_router.dart';
 import 'package:park_in/components/color_scheme.dart';
+import 'package:park_in/components/primary_btn.dart';
+import 'package:park_in/screens/drawer/about.dart';
+import 'package:park_in/screens/drawer/faqs.dart';
+import 'package:park_in/screens/drawer/report.dart';
+import 'package:park_in/screens/drawer/settings/change_password.dart';
+import 'package:park_in/screens/drawer/settings/personal_details.dart';
+import 'package:park_in/screens/drawer/settings/stickers.dart';
 import 'package:park_in/screens/home%20employee/notification_employee.dart';
 import 'package:park_in/screens/home%20employee/parking%20areas%20employee/parking_areas_4W_employee.dart';
 import 'package:park_in/screens/home%20student/parking%20areas%20student/parking_areas_2W_student.dart';
+import 'package:park_in/screens/sign%20in/sign_in_student_employee.dart';
+import 'package:park_in/services/auth/Auth_Service.dart';
 
 class HomeEmployeeScreen1 extends StatefulWidget {
   const HomeEmployeeScreen1({super.key});
@@ -15,10 +25,367 @@ class HomeEmployeeScreen1 extends StatefulWidget {
 
 class _HomeEmployeeScreen1State extends State<HomeEmployeeScreen1> {
   int value = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool _isLoading = false;
+
+  void logout(BuildContext context) async {
+    final authService = AuthService();
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      await authService.signOut();
+      Navigator.push(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (BuildContext context, Animation<double> animation1,
+              Animation<double> animation2) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(CurveTween(curve: Curves.fastEaseInToSlowEaseOut)
+                  .animate(animation1)),
+              child: const Material(
+                elevation: 5,
+                child: SignInScreen(),
+              ),
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 400),
+        ),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        backgroundColor: whiteColor,
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/images/bg1.png",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/images/AdNU_Logo.png",
+                        height: 70.h,
+                        width: 70.w,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4.h,
+                    ),
+                    Text(
+                      "Emmanuel",
+                      style: TextStyle(
+                        fontSize: 16.r,
+                        color: blackColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "2020000000",
+                      style: TextStyle(
+                        fontSize: 12.r,
+                        color: const Color.fromRGBO(27, 27, 27, 0.5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                physics: NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  Theme(
+                    data: Theme.of(context).copyWith(
+                      dividerColor: Colors.transparent,
+                    ),
+                    child: ExpansionTile(
+                      childrenPadding: EdgeInsets.only(left: 12.w),
+                      expansionAnimationStyle:
+                          AnimationStyle(curve: Curves.fastLinearToSlowEaseIn),
+                      leading: Icon(
+                        Icons.manage_accounts_outlined,
+                        color: blackColor,
+                        size: 24.r,
+                      ),
+                      title: Text(
+                        'Account Settings',
+                        style: TextStyle(fontSize: 12.sp),
+                      ),
+                      children: [
+                        ListTile(
+                          leading: Icon(
+                            Icons.account_box_outlined,
+                            color: blackColor,
+                            size: 24.r,
+                          ),
+                          title: Text(
+                            "Personal Details",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              color: blackColor,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (BuildContext context,
+                                    Animation<double> animation1,
+                                    Animation<double> animation2) {
+                                  return SlideTransition(
+                                    position: Tween<Offset>(
+                                      begin: const Offset(1, 0),
+                                      end: Offset.zero,
+                                    ).animate(CurveTween(
+                                            curve:
+                                                Curves.fastEaseInToSlowEaseOut)
+                                        .animate(animation1)),
+                                    child: const Material(
+                                      elevation: 5,
+                                      child: PersonalDetailsScreen(),
+                                    ),
+                                  );
+                                },
+                                transitionDuration:
+                                    const Duration(milliseconds: 400),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.shield_outlined,
+                            color: blackColor,
+                            size: 24.r,
+                          ),
+                          title: Text(
+                            "Change Password",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              color: blackColor,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (BuildContext context,
+                                    Animation<double> animation1,
+                                    Animation<double> animation2) {
+                                  return SlideTransition(
+                                    position: Tween<Offset>(
+                                      begin: const Offset(1, 0),
+                                      end: Offset.zero,
+                                    ).animate(CurveTween(
+                                            curve:
+                                                Curves.fastEaseInToSlowEaseOut)
+                                        .animate(animation1)),
+                                    child: const Material(
+                                      elevation: 5,
+                                      child: ChangePasswordScreen(),
+                                    ),
+                                  );
+                                },
+                                transitionDuration:
+                                    const Duration(milliseconds: 400),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.confirmation_num_outlined,
+                            color: blackColor,
+                            size: 24.r,
+                          ),
+                          title: Text(
+                            "Stickers",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              color: blackColor,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (BuildContext context,
+                                    Animation<double> animation1,
+                                    Animation<double> animation2) {
+                                  return SlideTransition(
+                                    position: Tween<Offset>(
+                                      begin: const Offset(1, 0),
+                                      end: Offset.zero,
+                                    ).animate(CurveTween(
+                                            curve:
+                                                Curves.fastEaseInToSlowEaseOut)
+                                        .animate(animation1)),
+                                    child: const Material(
+                                      elevation: 5,
+                                      child: StickersScreen(),
+                                    ),
+                                  );
+                                },
+                                transitionDuration:
+                                    const Duration(milliseconds: 400),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.flag_outlined,
+                      color: blackColor,
+                      size: 24.r,
+                    ),
+                    title: Text(
+                      'Report',
+                      style: TextStyle(fontSize: 12.sp),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (BuildContext context,
+                              Animation<double> animation1,
+                              Animation<double> animation2) {
+                            return SlideTransition(
+                              position: Tween<Offset>(
+                                begin: const Offset(1, 0),
+                                end: Offset.zero,
+                              ).animate(CurveTween(
+                                      curve: Curves.fastEaseInToSlowEaseOut)
+                                  .animate(animation1)),
+                              child: const Material(
+                                elevation: 5,
+                                child: ReportScreen(),
+                              ),
+                            );
+                          },
+                          transitionDuration: const Duration(milliseconds: 400),
+                        ),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.question_answer_outlined,
+                      color: blackColor,
+                      size: 24.r,
+                    ),
+                    title: Text(
+                      'FAQs',
+                      style: TextStyle(fontSize: 12.sp),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (BuildContext context,
+                              Animation<double> animation1,
+                              Animation<double> animation2) {
+                            return SlideTransition(
+                              position: Tween<Offset>(
+                                begin: const Offset(1, 0),
+                                end: Offset.zero,
+                              ).animate(CurveTween(
+                                      curve: Curves.fastEaseInToSlowEaseOut)
+                                  .animate(animation1)),
+                              child: const Material(
+                                elevation: 5,
+                                child: FaqsScreen(),
+                              ),
+                            );
+                          },
+                          transitionDuration: const Duration(milliseconds: 400),
+                        ),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.people_outline_rounded,
+                      color: blackColor,
+                      size: 24.r,
+                    ),
+                    title: Text(
+                      'About Park-in',
+                      style: TextStyle(fontSize: 12.sp),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (BuildContext context,
+                              Animation<double> animation1,
+                              Animation<double> animation2) {
+                            return SlideTransition(
+                              position: Tween<Offset>(
+                                begin: const Offset(1, 0),
+                                end: Offset.zero,
+                              ).animate(CurveTween(
+                                      curve: Curves.fastEaseInToSlowEaseOut)
+                                  .animate(animation1)),
+                              child: const Material(
+                                elevation: 5,
+                                child: AboutScreen(),
+                              ),
+                            );
+                          },
+                          transitionDuration: const Duration(milliseconds: 400),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+              child: PRKPrimaryBtn(
+                label: "Sign Out",
+                onPressed: () {
+                  logout(context);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      onDrawerChanged: (isOpened) {
+        if (!isOpened) {
+          NavbarNotifier.hideBottomNavBar = false;
+        }
+      },
       backgroundColor: bgColor,
       body: SafeArea(
         child: SingleChildScrollView(
@@ -38,7 +405,10 @@ class _HomeEmployeeScreen1State extends State<HomeEmployeeScreen1> {
                     splashColor: const Color.fromRGBO(45, 49, 250, 0.5),
                     highlightColor: const Color.fromRGBO(45, 49, 250, 0.5),
                     borderRadius: BorderRadius.circular(100),
-                    onTap: () {},
+                    onTap: () {
+                      _scaffoldKey.currentState?.openDrawer();
+                      NavbarNotifier.hideBottomNavBar = true;
+                    },
                     child: Card(
                       elevation: 0,
                       shape: const CircleBorder(),
