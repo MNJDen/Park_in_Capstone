0910c91c36d848a58b56a60680488f2c361c9e40
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-08-26 19:15:26 +0800

    Screens: Add Backend Home Employee

--- lib/screens/home employee/parking areas employee/parking_areas_4W_employee.dart
+++ lib/screens/home employee/parking areas employee/parking_areas_4W_employee.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:park_in/components/color_scheme.dart';
 import 'package:park_in/components/four%20wheels%20employee/alingal.dart';
 import 'package:park_in/components/four%20wheels%20employee/phelan.dart';
 
-class ParkingArea4WEmployee extends StatelessWidget {
+class ParkingArea4WEmployee extends StatefulWidget {
   const ParkingArea4WEmployee({super.key});
+
+  @override
+  State<ParkingArea4WEmployee> createState() => _ParkingArea4WEmployeeState();
+}
+
+class _ParkingArea4WEmployeeState extends State<ParkingArea4WEmployee> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref().child('parkingAreas');
+
+  int _alingalAvailableSpace = 0;
+  int _phelanAvailableSpace = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupListeners();
+  }
+
+  void _setupListeners() {
+    // Listen for changes to the Alingal parking area
+    _databaseReference.child('Alingal').onValue.listen((event) {
+      final DataSnapshot snapshot = event.snapshot;
+      if (snapshot.value != null) {
+        final Map<String, dynamic>? data =
+            (snapshot.value as Map?)?.cast<String, dynamic>();
+        if (mounted) {
+          setState(() {
+            _alingalAvailableSpace = data?['count'] ?? 0;
+          });
+        }
+      }
+    });
+
+    // Listen for changes to the Phelan parking area
+    _databaseReference.child('Phelan').onValue.listen((event) {
+      final DataSnapshot snapshot = event.snapshot;
+      if (snapshot.value != null) {
+        final Map<String, dynamic>? data =
+            (snapshot.value as Map?)?.cast<String, dynamic>();
+        if (mounted) {
+          setState(() {
+            _phelanAvailableSpace = data?['count'] ?? 0;
+          });
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,31 +78,31 @@ class ParkingArea4WEmployee extends StatelessWidget {
         SizedBox(
           height: 12.h,
         ),
-        const Row(
+        Row(
           children: [
             Expanded(
               child: PRKAlingal4WEmployee(
                 parkingArea: "Alingal",
-                availableSpace: "11",
+                availableSpace: _alingalAvailableSpace.toString(),
                 image: "assets/building_images/AlingalA-4W-E.png",
                 dotColor: parkingGreenColor,
               ),
-            )
+            ),
           ],
         ),
         SizedBox(
           height: 12.h,
         ),
-        const Row(
+        Row(
           children: [
             Expanded(
               child: PRKPhelan4WEmployee(
                 parkingArea: "Phelan",
-                availableSpace: "11",
+                availableSpace: _phelanAvailableSpace.toString(),
                 image: "assets/building_images/Phelan-4W-E.png",
                 dotColor: parkingYellowColor,
               ),
-            )
+            ),
           ],
         ),
         SizedBox(
