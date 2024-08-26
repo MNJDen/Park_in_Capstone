81669c496dc4ef1598e1a3566fd9c9b7f0661e7c
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-08-26 19:44:32 +0800

    Update: Notification Student

--- lib/screens/home student/notification_student.dart
+++ lib/screens/home student/notification_student.dart
@@ -113,6 +113,9 @@ class _NotificationStudentScreenState extends State<NotificationStudentScreen> {
                   }
                 },
               ),
+              SizedBox(
+                height: 100.h,
+              )
             ],
           ),
         ),
