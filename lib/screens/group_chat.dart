da1d5e1cfce26a04c13adf338707ee444613704c
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-08-29 18:26:17 +0800

    Screens: Update Group Chat

--- lib/screens/group_chat.dart
+++ lib/screens/group_chat.dart
@@ -21,6 +21,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final ChatService _chatService = ChatService();
   final TextEditingController _controller = TextEditingController();
   String? currentUserID;
+  Color _sendButtonColor = blackColor;
 
   @override
   void initState() {
@@ -47,13 +48,20 @@ class _ChatScreenState extends State<ChatScreen> {
     User? currentUser = FirebaseAuth.instance.currentUser;
 
     if (currentUser != null) {
+      // if may value ang currentUser ibig sabihon admin ang nakalog in
       setState(() {
-        currentUserID = currentUser.uid; // Store the uid in currentUserID
+        currentUserID = currentUser.uid; // Store the uid ng admin
       });
 
       _fetchUserData(); // Fetch user's data from 'User' collection
+    } else if (currentUser == null) {
+      //if nag null siya ibig sabihon bakong admin ang nakalog in
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      setState(() {
+        currentUserID = prefs.getString('userId'); // Store uid ng employee
+      });
     } else {
-      print('No user is currently logged in');
+      print("No user found");
     }
   }
 
@@ -105,7 +113,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
     final message = Message(
       senderID: currentUserID!,
-      name: name ?? 'Unknown', // unknown ang name if not found
+      name: name ?? 'Admin', // unknown ang name if not found
       message: content,
       timestamp: Timestamp.now(),
       userType: userType,
@@ -118,68 +126,80 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
         child: Column(
           children: [
             // header
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(
-                        context,
-                      );
-                      NavbarNotifier.hideBottomNavBar = false;
-                    },
-                    child: const Icon(
-                      Icons.arrow_back_ios_new_rounded,
-                      color: blackColor,
+            SizedBox(
+              height: 8.h,
+            ),
+            Container(
+              height: 60.h,
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(
+                          context,
+                        );
+                        NavbarNotifier.hideBottomNavBar = false;
+                      },
+                      child: const Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: blackColor,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  "Chat",
-                  style: TextStyle(
-                    fontSize: 20.r,
-                    color: blueColor,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    "Chat Room",
+                    style: TextStyle(
+                      fontSize: 20.r,
+                      color: blueColor,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             // header
 
             // body, the convo, tig rretrieve niya si messages from firebase
             Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: _chatService
-                    .getGroupMessagesStream(), //using this function in chat_service.dart tig rretrive niya si messages
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  final messages = snapshot.data!.docs;
-                  List<Widget> messageWidgets = messages.map((doc) {
-                    final data = doc.data() as Map<String, dynamic>;
-                    final messageText = data['message'] as String;
-                    final messageSender = data['name'] as String;
-                    final isCurrentUser = currentUserID == data['senderID'];
+              child: Container(
+                color: whiteColor,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: _chatService
+                      .getGroupMessagesStream(), //using this function in chat_service.dart tig rretrive niya si messages
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    final messages = snapshot.data!.docs;
+                    List<Widget> messageWidgets = messages.map((doc) {
+                      final data = doc.data() as Map<String, dynamic>;
+                      final messageText = data['message'] as String;
+                      final messageSender = data['name'] as String;
+                      final isCurrentUser = currentUserID == data['senderID'];
 
-                    // si values tig papasa niya ki ChatBubble (Chat_Bubble.dart). basically si Chat_Bubble sa UI man lang siya
-                    return ChatBubble(
-                      message: messageText,
-                      userName: messageSender,
-                      isCurrentUser: isCurrentUser,
+                      // si values tig papasa niya ki ChatBubble (Chat_Bubble.dart). basically si Chat_Bubble sa UI man lang siya
+                      return ChatBubble(
+                        message: messageText,
+                        userName: messageSender,
+                        isCurrentUser: isCurrentUser,
+                      );
+                    }).toList();
+
+                    return ListView(
+                      reverse: true,
+                      children: messageWidgets.reversed.toList(),
                     );
-                  }).toList();
-
-                  return ListView(
-                    children: messageWidgets,
-                  );
-                },
+                  },
+                ),
               ),
             ),
 
@@ -191,12 +211,49 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       controller: _controller,
-                      decoration:
-                          InputDecoration(hintText: 'Enter your message...'),
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        color: blackColor,
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: whiteColor,
+                        hintText: "Type your message here...",
+                        hintStyle: TextStyle(
+                          fontSize: 12.sp,
+                          color: const Color.fromARGB(90, 27, 27, 27),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 1.w,
+                            color: blueColor,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 0.5.w,
+                            color: borderBlack,
+                          ),
+                        ),
+                      ),
+                      onChanged: (text) {
+                        setState(
+                          () {
+                            if (text.trim().isEmpty) {
+                              _sendButtonColor = blackColor;
+                            } else {
+                              _sendButtonColor = blueColor;
+                            }
+                          },
+                        );
+                      },
                     ),
                   ),
                   IconButton(
                     icon: Icon(Icons.send),
+                    color: _sendButtonColor,
                     onPressed: () => sendMessage(_controller.text),
                   ),
                 ],
