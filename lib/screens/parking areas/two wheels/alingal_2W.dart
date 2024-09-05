f0098fac3ae93fd070a30cf85868f842845630f1
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-09-05 19:01:12 +0800

    Screens: Update Backend Alingal 2w

--- lib/screens/parking areas/two wheels/alingal_2W.dart
+++ lib/screens/parking areas/two wheels/alingal_2W.dart
@@ -1,9 +1,41 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:park_in/components/color_scheme.dart';
 
-class Alingal2w extends StatelessWidget {
+class Alingal2w extends StatefulWidget {
   const Alingal2w({super.key});
+
+  _Alingal2wState createState() => _Alingal2wState();
+}
+
+class _Alingal2wState extends State<Alingal2w> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref().child('parkingAreas');
+
+  int _alingal2wAvailableSpace = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _setupListeners();
+  }
+
+  void _setupListeners() {
+    _databaseReference.child('Alingal (M)').onValue.listen((event) {
+      final DataSnapshot snapshot = event.snapshot;
+      if (snapshot.value != null) {
+        final Map<String, dynamic>? data =
+            (snapshot.value as Map?)?.cast<String, dynamic>();
+        if (mounted) {
+          setState(() {
+            _alingal2wAvailableSpace = data?['count'] ?? 0;
+          });
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +109,9 @@ class Alingal2w extends StatelessWidget {
                             width: 7.w,
                             height: 7.w,
                             decoration: BoxDecoration(
-                              color: parkingGreenColor,
+                              color: _alingal2wAvailableSpace > 0
+                                  ? parkingGreenColor
+                                  : Colors.red,
                               borderRadius: BorderRadius.circular(100),
                             ),
                           ),
@@ -87,7 +121,7 @@ class Alingal2w extends StatelessWidget {
                   ),
                   const Spacer(),
                   Text(
-                    "11",
+                    "$_alingal2wAvailableSpace",
                     style: TextStyle(
                       fontSize: 52.sp,
                       color: blackColor,
