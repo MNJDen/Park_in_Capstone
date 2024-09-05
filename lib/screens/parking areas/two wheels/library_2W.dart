152f03f546f84bb5c151a2265247bddd28096056
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-09-05 19:01:26 +0800

    Screens: Update Backend Library 2w

--- lib/screens/parking areas/two wheels/library_2W.dart
+++ lib/screens/parking areas/two wheels/library_2W.dart
@@ -1,9 +1,42 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:park_in/components/color_scheme.dart';
 
-class Library2w extends StatelessWidget {
+class Library2w extends StatefulWidget {
   const Library2w({super.key});
+
+  _Library2wState createState() => _Library2wState();
+}
+
+class _Library2wState extends State<Library2w> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref().child('parkingAreas');
+
+  int _library2wAvailableSpace = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _setupListeners();
+  }
+
+  void _setupListeners() {
+    _databaseReference.child('Library (M)').onValue.listen((event) {
+      final DataSnapshot snapshot = event.snapshot;
+
+      if (snapshot.value != null) {
+        final Map<String, dynamic>? data =
+            (snapshot.value as Map?)?.cast<String, dynamic>();
+        if (mounted) {
+          setState(() {
+            _library2wAvailableSpace = data?['count'] ?? 0;
+          });
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +110,9 @@ class Library2w extends StatelessWidget {
                             width: 7.w,
                             height: 7.w,
                             decoration: BoxDecoration(
-                              color: parkingGreenColor,
+                              color: _library2wAvailableSpace > 0
+                                  ? parkingGreenColor
+                                  : Colors.red,
                               borderRadius: BorderRadius.circular(100),
                             ),
                           ),
@@ -87,7 +122,7 @@ class Library2w extends StatelessWidget {
                   ),
                   const Spacer(),
                   Text(
-                    "11",
+                    "$_library2wAvailableSpace",
                     style: TextStyle(
                       fontSize: 52.sp,
                       color: blackColor,
