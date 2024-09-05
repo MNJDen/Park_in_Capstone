90142af8772ac439c2092c98951c7cf78d6d0038
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-09-05 19:01:49 +0800

    Screens: Update Backend Change Password

--- lib/screens/drawer/settings/change_password.dart
+++ lib/screens/drawer/settings/change_password.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:park_in/components/color_scheme.dart';
 import 'package:park_in/components/form_field.dart';
 import 'package:park_in/components/primary_btn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({super.key});
@@ -15,6 +17,57 @@ class ChangePasswordScreennState extends State<ChangePasswordScreen> {
   final TextEditingController _oldPasswordCtrl = TextEditingController();
   final TextEditingController _newPasswordCtrl = TextEditingController();
   final TextEditingController _confirmPasswordCtrl = TextEditingController();
+
+  Future<String?> _getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userId');
+  }
+
+  void _checkUpdatePassword() async {
+    final userId = await _getUserId();
+    if (userId != null) {
+      final docSnapshot =
+          await FirebaseFirestore.instance.collection('User').doc(userId).get();
+      String oldPassword = docSnapshot['password'];
+
+      if (oldPassword == _oldPasswordCtrl.text &&
+          _newPasswordCtrl.text == _confirmPasswordCtrl.text) {
+        await FirebaseFirestore.instance
+            .collection('User')
+            .doc(userId)
+            .update({'password': _confirmPasswordCtrl.text});
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Password updated successfully!'),
+            backgroundColor: Colors.green,
+          ),
+        );
+
+        _oldPasswordCtrl.clear();
+        _newPasswordCtrl.clear();
+        _confirmPasswordCtrl.clear();
+      } else {
+        if (oldPassword != _oldPasswordCtrl.text) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Old password is incorrect.'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else if (_newPasswordCtrl.text != _confirmPasswordCtrl.text) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('New password does not match.'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      }
+    } else {
+      print('Error: User ID not found.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,7 +152,9 @@ class ChangePasswordScreennState extends State<ChangePasswordScreen> {
                 padding: EdgeInsets.only(bottom: 40.h),
                 child: PRKPrimaryBtn(
                   label: "Confirm",
-                  onPressed: () {},
+                  onPressed: () {
+                    _checkUpdatePassword();
+                  },
                 ),
               ),
             ],
