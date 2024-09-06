358ca81e61e017363db9023413aad59793fea8e6
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-06 17:21:20 +0800

    Screens: Update Home Student 1

--- lib/screens/home student/home_student1.dart
+++ lib/screens/home student/home_student1.dart
@@ -122,34 +122,33 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
                   child: Center(
                     child: Column(
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
+                        ClipOval(
                           child: Image.network(
                             profilePicture,
                             height: 70.h,
-                            width: 70.w,
+                            width: 70.h,
+                            fit: BoxFit.cover,
                             loadingBuilder: (context, child, progress) {
                               if (progress == null) {
-                                return child; // Image is loaded
+                                return child;
                               } else {
                                 return Center(
                                   child: CircularProgressIndicator(
-                                    strokeWidth:
-                                        2.0, // Adjust the stroke width to make it small
+                                    strokeWidth: 2.0,
                                     value: progress.expectedTotalBytes != null
                                         ? progress.cumulativeBytesLoaded /
                                             (progress.expectedTotalBytes ?? 1)
                                         : null,
                                   ),
-                                ); // Small circular progress indicator while loading
+                                );
                               }
                             },
                             errorBuilder: (context, error, stackTrace) {
                               return Image.asset(
                                 "assets/images/AdNU_Logo.png",
                                 height: 70.h,
-                                width: 70.w,
-                              ); // Fallback image in case of error
+                                width: 70.h,
+                              );
                             },
                           ),
                         ),
@@ -177,7 +176,7 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
                 ),
                 Expanded(
                   child: ListView(
-                    physics: NeverScrollableScrollPhysics(),
+                    physics: const NeverScrollableScrollPhysics(),
                     padding: EdgeInsets.zero,
                     children: <Widget>[
                       Theme(
@@ -432,6 +431,9 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
                     ],
                   ),
                 ),
+                Divider(
+                  thickness: 0.5.w,
+                ),
                 Padding(
                   padding:
                       EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
@@ -460,89 +462,22 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 32.h,
+                height: 20.h,
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  InkWell(
-                    enableFeedback: true,
+                  IconButton(
                     splashColor: const Color.fromRGBO(45, 49, 250, 0.5),
                     highlightColor: const Color.fromRGBO(45, 49, 250, 0.5),
-                    borderRadius: BorderRadius.circular(100),
-                    onTap: () {
+                    onPressed: () {
                       _scaffoldKey.currentState?.openDrawer();
                       NavbarNotifier.hideBottomNavBar = true;
                     },
-                    child: Card(
-                      elevation: 0,
-                      shape: const CircleBorder(),
-                      clipBehavior: Clip.antiAlias,
-                      child:
-                          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                        stream: userSubject.stream,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          } else if (snapshot.hasData) {
-                            return StreamBuilder<
-                                DocumentSnapshot<Map<String, dynamic>>>(
-                              stream: userSubject.stream,
-                              builder: (context, snapshot) {
-                                if (snapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return CircularProgressIndicator();
-                                } else if (snapshot.hasError) {
-                                  return Icon(Icons.error, color: blackColor);
-                                } else if (snapshot.hasData) {
-                                  final profilePicUrl =
-                                      snapshot.data?['profilePicture'] ??
-                                          "assets/images/default_profile.png";
-                                  return Image.network(
-                                    profilePicUrl,
-                                    fit: BoxFit.fill,
-                                    height: 40.h,
-                                    width: 40.w,
-                                    loadingBuilder: (context, child, progress) {
-                                      if (progress == null) {
-                                        return child; // Image is loaded
-                                      } else {
-                                        return Center(
-                                          child: CircularProgressIndicator(
-                                            strokeWidth:
-                                                2.0, // Adjust the stroke width to make it small
-                                            value: progress
-                                                        .expectedTotalBytes !=
-                                                    null
-                                                ? progress
-                                                        .cumulativeBytesLoaded /
-                                                    (progress
-                                                            .expectedTotalBytes ??
-                                                        1)
-                                                : null,
-                                          ),
-                                        ); // Small circular progress indicator while loading
-                                      }
-                                    },
-                                    errorBuilder: (context, error, stackTrace) {
-                                      return Image.asset(
-                                        "assets/images/AdNU_Logo.png",
-                                        height: 70.h,
-                                        width: 70.w,
-                                      ); // Fallback image in case of error
-                                    },
-                                  );
-                                } else {
-                                  return Icon(Icons.error, color: blackColor);
-                                }
-                              },
-                            );
-                          } else {
-                            return Icon(Icons.error, color: blackColor);
-                          }
-                        },
-                      ),
+                    icon: const Icon(
+                      Icons.menu_rounded,
+                      color: blackColor,
+                      size: 30,
                     ),
                   ),
                   SizedBox(
@@ -555,22 +490,49 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
                       builder: (context, snapshot) {
                         if (snapshot.connectionState ==
                             ConnectionState.waiting) {
-                          return CircularProgressIndicator();
-                        } else if (snapshot.hasData && snapshot.data != null) {
-                          final userData = snapshot.data!;
-                          final String name =
-                              userData['name'] ?? 'Hello, User!';
                           return Text(
-                            "Hello, $name!",
+                            'Hello, ---!',
                             style: TextStyle(
                               fontSize: 20.r,
                               fontWeight: FontWeight.bold,
                               color: blackColor,
                             ),
                           );
+                        } else if (snapshot.hasData && snapshot.data != null) {
+                          final userData = snapshot.data!;
+                          final String name =
+                              userData['name'] ?? 'Hello, User!';
+                          return Wrap(
+                            children: [
+                              Text(
+                                "Hello, ",
+                                style: TextStyle(
+                                  fontSize: 20.r,
+                                  fontWeight: FontWeight.bold,
+                                  color: blackColor,
+                                ),
+                              ),
+                              Text(
+                                "$name",
+                                style: TextStyle(
+                                  fontSize: 20.r,
+                                  fontWeight: FontWeight.bold,
+                                  color: blueColor,
+                                ),
+                              ),
+                              Text(
+                                "!",
+                                style: TextStyle(
+                                  fontSize: 20.r,
+                                  fontWeight: FontWeight.bold,
+                                  color: blackColor,
+                                ),
+                              ),
+                            ],
+                          );
                         } else {
                           return Text(
-                            'Hello, User!',
+                            'Hello, ---!',
                             style: TextStyle(
                               fontSize: 20.r,
                               fontWeight: FontWeight.bold,
@@ -667,7 +629,7 @@ class _HomeStudentScreen1State extends State<HomeStudentScreen1> {
                 ],
               ),
               SizedBox(
-                height: 28.h,
+                height: 20.h,
               ),
               AnimatedToggleSwitch<int>.size(
                 current: value,
