52f138f5fcdcc777e2b45e8d79437a3f4b58a84a
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-06 17:16:11 +0800

    Components: Update Chat Bubble

--- lib/components/chat/Chat_Bubble.dart
+++ lib/components/chat/Chat_Bubble.dart
@@ -17,7 +17,7 @@ class ChatBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var alignment =
-        isCurrentUser ? Alignment.centerRight : Alignment.bottomLeft;
+        isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
 
     return Column(
       crossAxisAlignment:
@@ -26,44 +26,54 @@ class ChatBubble extends StatelessWidget {
         Container(
           alignment: alignment,
           margin: EdgeInsets.symmetric(vertical: 2.h, horizontal: 15.w),
-          child: Column(
-            crossAxisAlignment: isCurrentUser
-                ? CrossAxisAlignment.end
-                : CrossAxisAlignment.start,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               if (!isCurrentUser)
-                Text(
-                  userName,
-                  style: TextStyle(
-                    fontSize: 10.sp,
-                    color: const Color.fromARGB(255, 129, 129, 129),
-                  ),
-                ),
-              SizedBox(
-                height: 2.h,
+              CircleAvatar(
+                radius: 14.w,
               ),
-              Container(
-                margin: isCurrentUser
-                    ? EdgeInsets.only(left: 50.w)
-                    : EdgeInsets.only(right: 50.w),
-                child: Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
-                  decoration: BoxDecoration(
-                    color: isCurrentUser
-                        ? blueColor
-                        : Color.fromRGBO(240, 240, 240, 1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    message,
-                    style: TextStyle(
-                      fontSize: 12.sp,
-                      color: isCurrentUser
-                          ? whiteColor
-                          : blackColor,
+              SizedBox(
+                  width: isCurrentUser
+                      ? 0
+                      : 8.w), 
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: isCurrentUser
+                      ? CrossAxisAlignment.end
+                      : CrossAxisAlignment.start,
+                  children: [
+                    if (!isCurrentUser)
+                      Text(
+                        userName,
+                        style: TextStyle(
+                          fontSize: 10.sp,
+                          color: const Color.fromARGB(255, 129, 129, 129),
+                        ),
+                      ),
+                    SizedBox(height: 2.h),
+                    Container(
+                      margin: isCurrentUser
+                          ? EdgeInsets.only(left: 50.w)
+                          : EdgeInsets.only(right: 50.w),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
+                      decoration: BoxDecoration(
+                        color: isCurrentUser
+                            ? blueColor
+                            : const Color.fromRGBO(240, 240, 240, 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        message,
+                        style: TextStyle(
+                          fontSize: 12.sp,
+                          color: isCurrentUser ? whiteColor : blackColor,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ],
