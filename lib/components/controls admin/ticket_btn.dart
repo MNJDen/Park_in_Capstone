053b3086a64a597b7ba79a45b82bbc224a63c10d
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-06 17:16:58 +0800

    Components: Add Ticket Button

---
+++ lib/components/controls admin/ticket_btn.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:park_in/components/theme/color_scheme.dart';
+import 'package:park_in/screens/home%20admin/cite_ticket_admin.dart';
+
+class PRKTicket extends StatefulWidget {
+  const PRKTicket({super.key});
+
+  @override
+  State<PRKTicket> createState() => _PRKTicketState();
+}
+
+class _PRKTicketState extends State<PRKTicket> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _colorAnimation = ColorTween(
+      begin: whiteColor,
+      end: const Color.fromARGB(255, 209, 210, 253),
+    ).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!_animationController.isAnimating) {
+          _animationController.forward(from: 0);
+          Timer(
+            Duration(milliseconds: 350),
+            () {
+              _animationController.reverse();
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (BuildContext context,
+                      Animation<double> animation1,
+                      Animation<double> animation2) {
+                    return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(1, 0),
+                        end: Offset.zero,
+                      ).animate(
+                          CurveTween(curve: Curves.fastEaseInToSlowEaseOut)
+                              .animate(animation1)),
+                      child: const Material(
+                        elevation: 5,
+                        child: CiteTicketAdminScreen(),
+                      ),
+                    );
+                  },
+                  transitionDuration: const Duration(milliseconds: 400),
+                ),
+              );
+            },
+          );
+        }
+      },
+      child: AnimatedBuilder(
+        animation: _colorAnimation,
+        builder: (context, child) {
+          return Container(
+            width: 98.w,
+            height: 74.h,
+            decoration: BoxDecoration(
+              color: _colorAnimation.value,
+              borderRadius: BorderRadius.circular(
+                10,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 8,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.receipt_long_outlined,
+                  size: 24.r,
+                  color: blackColor,
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Text(
+                  "Ticket",
+                  style: TextStyle(
+                    color: blackColor,
+                    fontSize: 12.r,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
