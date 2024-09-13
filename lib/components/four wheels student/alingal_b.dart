7acd5f469b9798080ba7e26edd898479006b4eba
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-13 16:29:06 +0800

    Components: Update Alingal B 4W Student

--- lib/components/four wheels student/alingal_b.dart
+++ lib/components/four wheels student/alingal_b.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:navbar_router/navbar_router.dart';
 import 'package:park_in/components/theme/color_scheme.dart';
 import 'package:park_in/screens/parking%20areas/four%20wheels%20student/alingal_B_4W.dart';
 
@@ -79,6 +80,7 @@ class _PRKAlingalB4WStundentState extends State<PRKAlingalB4WStundent>
                   transitionDuration: const Duration(milliseconds: 400),
                 ),
               );
+              NavbarNotifier.hideBottomNavBar = true;
             },
           );
         }
