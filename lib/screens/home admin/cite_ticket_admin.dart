c9cf58d479b12175132e86a16a10be31aba5110e
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-13 16:32:23 +0800

    Screens: Update Cite Ticket Admin

--- lib/screens/home admin/cite_ticket_admin.dart
+++ lib/screens/home admin/cite_ticket_admin.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:park_in/components/controls%20admin/ticket_upload.dart';
 import 'package:park_in/components/theme/color_scheme.dart';
 import 'package:park_in/components/field/form_field.dart';
 import 'package:park_in/components/ui/primary_btn.dart';
@@ -23,27 +24,70 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
   final TextEditingController _vehicleTypeCtrl = TextEditingController();
   final TextEditingController _searchCtrl = TextEditingController();
   final TextEditingController _descriptionCtrl = TextEditingController();
-  File? _selectedImage;
+  File? _closeUpImage;
+  File? _midShotImage;
+  File? _wideShotImage;
 
   void _onSuggestionTap(String suggestion) {
     setState(() {});
   }
 
-  Future<void> _pickImage() async {
+  Future<void> _pickCloseUpImage() async {
     final ImagePicker picker = ImagePicker();
     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
     if (image != null) {
       setState(() {
-        _selectedImage = File(image.path);
+        _closeUpImage = File(image.path);
       });
     }
   }
 
-  // Method to clear the selected image
+  Future<void> _pickMidShotImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        _midShotImage = File(image.path);
+      });
+    }
+  }
+
+  Future<void> _pickWideShotImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        _wideShotImage = File(image.path);
+      });
+    }
+  }
+
   void _clearImage() {
     setState(() {
-      _selectedImage = null; // Clear the selected image
+      _closeUpImage = null;
+      _midShotImage = null;
+      _wideShotImage = null;
+    });
+  }
+
+  void _clearCloseUpImage() {
+    setState(() {
+      _closeUpImage = null;
+    });
+  }
+
+  void _clearMidShotImage() {
+    setState(() {
+      _midShotImage = null;
+    });
+  }
+
+  void _clearWideShotImage() {
+    setState(() {
+      _wideShotImage = null;
     });
   }
 
@@ -56,7 +100,6 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
       await ref.putFile(image);
       return await ref.getDownloadURL();
     } catch (e) {
-      // Handle error, e.g., show a Snackbar or log the error
       return null;
     }
   }
@@ -64,31 +107,195 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
   Future<void> _citeTicket() async {
     if (_plateNumberCtrl.text.isEmpty ||
         _vehicleTypeCtrl.text.isEmpty ||
-        _searchCtrl.text.isEmpty ||
-        _descriptionCtrl.text.isEmpty) {
-      // Show an error, e.g., a Snackbar or AlertDialog
+        _searchCtrl.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          width: MediaQuery.of(context).size.width * 0.95,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: const Color.fromARGB(255, 255, 226, 226),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(
+              color: Color.fromRGBO(255, 0, 0, 1),
+            ),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline_rounded,
+                color: const Color.fromRGBO(255, 0, 0, 1),
+                size: 20.r,
+              ),
+              SizedBox(
+                width: 8.w,
+              ),
+              Flexible(
+                child: Text(
+                  "Fill out all the required fields",
+                  style: TextStyle(
+                    color: blackColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
       return;
     }
 
-    String? imageUrl;
-    if (_selectedImage != null) {
-      imageUrl = await _uploadImage(_selectedImage!);
+    if (_closeUpImage == null ||
+        _midShotImage == null ||
+        _wideShotImage == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          width: MediaQuery.of(context).size.width * 0.95,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: const Color.fromARGB(255, 255, 226, 226),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(
+              color: Color.fromRGBO(255, 0, 0, 1),
+            ),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline_rounded,
+                color: const Color.fromRGBO(255, 0, 0, 1),
+                size: 20.r,
+              ),
+              SizedBox(
+                width: 8.w,
+              ),
+              Flexible(
+                child: Text(
+                  "Please upload all required images (Close-up, Mid Shot, Wide Shot)",
+                  style: TextStyle(
+                    color: blackColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+      return;
     }
 
-    await FirebaseFirestore.instance.collection('Violation Ticket').add({
-      'plate_number': _plateNumberCtrl.text,
-      'vehicle_type': _vehicleTypeCtrl.text,
-      'violation': _searchCtrl.text,
-      'description': _descriptionCtrl.text,
-      'image_url': imageUrl,
-      'timestamp': FieldValue.serverTimestamp(),
-    });
+    String? closeUpImageUrl, midShotImageUrl, wideShotImageUrl;
+    if (_closeUpImage != null) {
+      closeUpImageUrl = await _uploadImage(_closeUpImage!);
+    }
+    if (_midShotImage != null) {
+      midShotImageUrl = await _uploadImage(_midShotImage!);
+    }
+    if (_wideShotImage != null) {
+      wideShotImageUrl = await _uploadImage(_wideShotImage!);
+    }
 
-    _plateNumberCtrl.clear();
-    _vehicleTypeCtrl.clear();
-    _searchCtrl.clear();
-    _descriptionCtrl.clear();
-    _clearImage();
+    try {
+      await FirebaseFirestore.instance.collection('Violation Ticket').add({
+        'plate_number': _plateNumberCtrl.text,
+        'vehicle_type': _vehicleTypeCtrl.text,
+        'violation': _searchCtrl.text,
+        'description': _descriptionCtrl.text,
+        'close_up_image_url': closeUpImageUrl,
+        'mid_shot_image_url': midShotImageUrl,
+        'wide_shot_image_url': wideShotImageUrl,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      _plateNumberCtrl.clear();
+      _vehicleTypeCtrl.clear();
+      _searchCtrl.clear();
+      _descriptionCtrl.clear();
+      _clearImage();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          width: MediaQuery.of(context).size.width * 0.95,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: const Color.fromRGBO(217, 255, 214, 1),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(
+              color: Color.fromRGBO(20, 255, 0, 1),
+            ),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.check_circle_outline_rounded,
+                color: const Color.fromRGBO(20, 255, 0, 1),
+                size: 20.r,
+              ),
+              SizedBox(
+                width: 8.w,
+              ),
+              Flexible(
+                child: Text(
+                  "Ticket cited successfully",
+                  style: TextStyle(
+                    color: blackColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          width: MediaQuery.of(context).size.width * 0.95,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: const Color.fromARGB(255, 255, 226, 226),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(
+              color: Color.fromRGBO(255, 0, 0, 1),
+            ),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline_rounded,
+                color: const Color.fromRGBO(255, 0, 0, 1),
+                size: 20.r,
+              ),
+              SizedBox(
+                width: 8.w,
+              ),
+              Flexible(
+                child: Text(
+                  "Error Occured: $e",
+                  style: TextStyle(
+                    color: blackColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -105,7 +312,7 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 32.h),
+                  SizedBox(height: 20.h),
                   Stack(
                     alignment: Alignment.center,
                     children: [
@@ -140,10 +347,17 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
                     controller: _plateNumberCtrl,
                   ),
                   SizedBox(height: 12.h),
-                  PRKFormField(
+                  PRKSearchField(
                     prefixIcon: Icons.bike_scooter_rounded,
                     labelText: "Vehicle Type",
+                    searchFieldListItems: [
+                      SearchFieldListItem('Two-Wheels'),
+                      SearchFieldListItem('Four-Wheels'),
+                    ],
                     controller: _vehicleTypeCtrl,
+                    onTap: (text) {
+                      _onSuggestionTap(text);
+                    },
                   ),
                   SizedBox(height: 12.h),
                   PRKSearchField(
@@ -167,7 +381,7 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
                   ),
                   SizedBox(height: 12.h),
                   PRKTextArea(
-                    labelText: "Description",
+                    labelText: "Description (Optional)",
                     controller: _descriptionCtrl,
                   ),
                   SizedBox(height: 12.h),
@@ -180,26 +394,47 @@ class _CiteTicketAdminScreenState extends State<CiteTicketAdminScreen> {
                           fontSize: 12.r,
                         ),
                       ),
-                      const Spacer(),
-                      GestureDetector(
-                        onTap: _pickImage,
-                        child: Icon(
-                          Icons.attachment_rounded,
-                          color: blackColor,
-                          size: 24.r,
-                        ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  Column(
+                    children: [
+                      PRKTicketsUpload(
+                        label: "Close-up Shot",
+                        onPressed: _closeUpImage != null
+                            ? _clearCloseUpImage
+                            : _pickCloseUpImage,
+                        image: _closeUpImage,
+                        icon: _closeUpImage != null
+                            ? Icons.highlight_remove_rounded
+                            : Icons.file_upload_outlined,
+                      ),
+                      SizedBox(height: 8.h),
+                      PRKTicketsUpload(
+                        label: "Mid Shot",
+                        onPressed: _midShotImage != null
+                            ? _clearMidShotImage
+                            : _pickMidShotImage,
+                        image: _midShotImage,
+                        icon: _midShotImage != null
+                            ? Icons.highlight_remove_rounded
+                            : Icons.file_upload_outlined,
+                      ),
+                      SizedBox(height: 8.h),
+                      PRKTicketsUpload(
+                        label: "Wide Shot",
+                        onPressed: _wideShotImage != null
+                            ? _clearWideShotImage
+                            : _pickWideShotImage,
+                        image: _wideShotImage,
+                        icon: _wideShotImage != null
+                            ? Icons.highlight_remove_rounded
+                            : Icons.file_upload_outlined,
                       ),
                     ],
                   ),
-                  if (_selectedImage != null)
-                    Padding(
-                      padding: EdgeInsets.only(top: 10.h),
-                      child: Image.file(
-                        _selectedImage!,
-                        height: 100.h,
-                        width: 100.h,
-                      ),
-                    ),
                 ],
               ),
               Padding(
