2ee35b4762c16dda13bffd1a9925833fe132a869
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-13 16:29:24 +0800

    Components: Update Coko Cafe 4W Student

--- lib/components/four wheels student/coko_cafe.dart
+++ lib/components/four wheels student/coko_cafe.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:navbar_router/navbar_router.dart';
 import 'package:park_in/components/theme/color_scheme.dart';
 import 'package:park_in/screens/parking%20areas/four%20wheels%20student/Coko_4W.dart';
 
@@ -79,6 +80,7 @@ class _PRKCoko4WStundentState extends State<PRKCoko4WStundent>
                   transitionDuration: const Duration(milliseconds: 400),
                 ),
               );
+              NavbarNotifier.hideBottomNavBar = true;
             },
           );
         }
