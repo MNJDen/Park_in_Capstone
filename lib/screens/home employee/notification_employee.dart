0a0059fc57d4c9fe25839b8c4acd9773d6348211
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-10-01 21:43:51 +0800

    Screens: Update Backend Notification Employee

--- lib/screens/home employee/notification_employee.dart
+++ lib/screens/home employee/notification_employee.dart
@@ -56,7 +56,7 @@ class _NotificationStudeEmployeenState
           .doc(widget.userId) // Get the document with the current user's ID
           .get();
 
-      final plateNoField = userDoc['plateNo']; // This might be a List or String
+      final plateNoField = userDoc['plateNo'];
 
       setState(() {
         if (plateNoField is List) {
@@ -151,6 +151,7 @@ class _NotificationStudeEmployeenState
                             stream: FirebaseFirestore.instance
                                 .collection('Violation Ticket')
                                 .where('plate_number', whereIn: _plateNumbers)
+                                .where('status', isEqualTo: 'Pending')
                                 .snapshots(),
                             builder: (context, snapshot) {
                               if (snapshot.connectionState ==
