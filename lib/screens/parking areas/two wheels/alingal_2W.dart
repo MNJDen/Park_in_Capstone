f0022e98eaff1b49d5c92c4406a0c48c1f207eb6
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-10-01 21:46:16 +0800

    Screens: Update Backend Alingal 2W

--- lib/screens/parking areas/two wheels/alingal_2W.dart
+++ lib/screens/parking areas/two wheels/alingal_2W.dart
@@ -37,6 +37,42 @@ class _Alingal2wState extends State<Alingal2w> {
     });
   }
 
+  final int _maxSpace = 80;
+
+  Color _getStatusColor() {
+    if (_alingal2wAvailableSpace == 0) {
+      return parkingRedColor; // Red when full.
+    } else if (_alingal2wAvailableSpace == _maxSpace) {
+      return parkingGreenColor; // Green when completely empty.
+    } else if (_alingal2wAvailableSpace < 80 && _alingal2wAvailableSpace > 40) {
+      return parkingGreenColor;
+    } else if (_alingal2wAvailableSpace <= 40 &&
+        _alingal2wAvailableSpace > 10) {
+      return parkingYellowColor; // Yellow when mid-way full.
+    } else if (_alingal2wAvailableSpace <= 10) {
+      return parkingOrangeColor; // Orange when almost full.
+    } else {
+      return parkingYellowColor;
+    }
+  }
+
+  String _getStatusText() {
+    if (_alingal2wAvailableSpace == 0) {
+      return "Full"; // Red status.
+    } else if (_alingal2wAvailableSpace == _maxSpace) {
+      return "Available"; // Green status.
+    } else if (_alingal2wAvailableSpace < 80 && _alingal2wAvailableSpace > 40) {
+      return "Available"; // Green status.
+    } else if (_alingal2wAvailableSpace <= 40 &&
+        _alingal2wAvailableSpace > 10) {
+      return "Mid-way Full"; // Yellow status.
+    } else if (_alingal2wAvailableSpace <= 10) {
+      return "Almost Full"; // Orange status.
+    } else {
+      return "Filling";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,15 +155,13 @@ class _Alingal2wState extends State<Alingal2w> {
                                   vertical: 2.h,
                                 ),
                                 decoration: BoxDecoration(
-                                  color: _alingal2wAvailableSpace > 0
-                                      ? parkingGreenColor.withOpacity(0.1)
-                                      : Colors.red,
+                                  color: _getStatusColor().withOpacity(0.1),
                                   borderRadius: BorderRadius.circular(100),
                                 ),
                                 child: Text(
-                                  "Green",
+                                  _getStatusText(),
                                   style: TextStyle(
-                                    color: parkingGreenColor,
+                                    color: _getStatusColor(),
                                     fontSize: 12.sp,
                                     fontWeight: FontWeight.w400,
                                   ),
