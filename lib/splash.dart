a5b393b79fb1cfdad452ca093fa0de5644ae4ec6
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-12-07 18:11:39 +0800

    Screens: Update Splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -96,6 +96,7 @@ class _SplashScreenState extends State<SplashScreen>
                 child: Image.asset(
                   "assets/images/Logo.png",
                   height: 73.h,
+                  color: blueColor,
                 ),
               ),
               Positioned(
