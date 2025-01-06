858f9a84c227ab701cf98ead56ebba4eda650919
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2025-01-06 14:59:56 +0800

    Screens: Update Backend Page Indicator Employee

--- lib/screens/sign up/sign up employee/page_indicator_employee.dart
+++ lib/screens/sign up/sign up employee/page_indicator_employee.dart
@@ -23,47 +23,155 @@ class _PageIndicatorEmployeeState extends State<PageIndicatorEmployee> {
   late final List<Widget> _pages;
 
   // GlobalKey for specific screens where user data needs to be updated
-  final GlobalKey<SignUpEmployeeScreen5State> _signUpEmployeeScreen5Key =
-      GlobalKey<SignUpEmployeeScreen5State>();
-
+  final GlobalKey<SignUpEmployeeScreen1State> _signUpEmployeeScreen1Key =
+      GlobalKey<SignUpEmployeeScreen1State>();
+  final GlobalKey<SignUpEmployeeScreen2State> _signUpEmployeeScreen2Key =
+      GlobalKey<SignUpEmployeeScreen2State>();
+  final GlobalKey<SignUpEmployeeScreen3State> _signUpEmployeeScreen3Key =
+      GlobalKey<SignUpEmployeeScreen3State>();
   final GlobalKey<SignUpEmployeeScreen4State> _signUpEmployeeScreen4Key =
       GlobalKey<SignUpEmployeeScreen4State>();
+  final GlobalKey<SignUpEmployeeScreen5State> _signUpEmployeeScreen5Key =
+      GlobalKey<SignUpEmployeeScreen5State>();
+  final GlobalKey<SignUpEmployeeScreen6State> _signUpEmployeeScreen6Key =
+      GlobalKey<SignUpEmployeeScreen6State>();
+
+  bool _isNextButtonEnabled = false;
 
   @override
   void initState() {
     super.initState();
+
+    // Initialize pages with the required callbacks
     _pages = [
-      const SignUpEmployeeScreen1(),
-      const SignUpEmployeeScreen2(),
-      const SignUpEmployeeScreen3(),
-      SignUpEmployeeScreen4(key: _signUpEmployeeScreen4Key),
-      SignUpEmployeeScreen5(key: _signUpEmployeeScreen5Key),
-      const SignUpEmployeeScreen6(),
+      SignUpEmployeeScreen1(
+        key: _signUpEmployeeScreen1Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
+      SignUpEmployeeScreen2(
+        key: _signUpEmployeeScreen2Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
+      SignUpEmployeeScreen3(
+        key: _signUpEmployeeScreen3Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
+      SignUpEmployeeScreen4(
+        key: _signUpEmployeeScreen4Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
+      SignUpEmployeeScreen5(
+        key: _signUpEmployeeScreen5Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
+      SignUpEmployeeScreen6(
+        key: _signUpEmployeeScreen6Key,
+        onFormValidityChanged: (isValid) {
+          // Call setState safely after the build phase
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() => _isNextButtonEnabled = isValid);
+          });
+        },
+      ),
     ];
+
+    // Schedule validation of the current page after build phase
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _validateCurrentPage(); // Validate the form after widgets are mounted
+    });
   }
 
   void _updateUserData() {
     switch (_currentPage) {
       case 0:
-        // SignUpEmployeeScreen1 automatically updates the provider
+        _signUpEmployeeScreen1Key.currentState;
         break;
       case 1:
-        // SignUpEmployeeScreen2 automatically updates the provider
+        _signUpEmployeeScreen2Key.currentState;
         break;
       case 2:
-        // Access the _updateProviderData method via the global key
+        _signUpEmployeeScreen3Key.currentState;
+        break;
+      case 3:
+        _signUpEmployeeScreen4Key.currentState?.updateProviderData();
+        break;
+      case 4:
+        _signUpEmployeeScreen5Key.currentState?.updateProviderData();
+        break;
+      case 5:
+        _signUpEmployeeScreen6Key.currentState;
+        break;
+    }
+  }
+
+  void _validateCurrentPage() {
+    switch (_currentPage) {
+      case 0:
+        final state = _signUpEmployeeScreen1Key.currentState;
+        if (state != null) {
+          print('isFormValid: ${state.isFormValid()}'); // Debug log
+          setState(() => _isNextButtonEnabled = state.isFormValid());
+        } else {
+          print('State is null for SignUpEmployeeScreen1');
+        }
+      case 1:
+        final state = _signUpEmployeeScreen2Key.currentState;
+        final isValid = state?.isFormValid() ?? false;
+        print('Page 2 - isFormValid: $isValid');
+        setState(() => _isNextButtonEnabled = isValid);
+        break;
+      case 2:
+        final state = _signUpEmployeeScreen3Key.currentState;
+        final isValid = state?.isFormValid() ?? false;
+        print('Page 3 - isFormValid: $isValid');
+        setState(() => _isNextButtonEnabled = isValid);
         break;
       case 3:
         final state = _signUpEmployeeScreen4Key.currentState;
-        state?.updateProviderData();
+        final isValid = state?.isFormValid() ?? false;
+        print('Page 4 - isFormValid: $isValid');
+        setState(() => _isNextButtonEnabled = isValid);
         break;
       case 4:
         final state = _signUpEmployeeScreen5Key.currentState;
-        state?.updateProviderData();
+        final isValid = state?.isFormValid() ?? false;
+        print('Page 5 - isFormValid: $isValid');
+        setState(() => _isNextButtonEnabled = isValid);
         break;
       case 5:
+        final state = _signUpEmployeeScreen6Key.currentState;
+        final isValid = state?.isFormValid() ?? false;
+        print('Page 6 - isFormValid: $isValid');
+        setState(() => _isNextButtonEnabled = isValid);
         break;
-      // Add cases for other screens as needed
+      default:
+        print('Unknown page: $_currentPage');
+        break;
     }
   }
 
@@ -104,58 +212,63 @@ class _PageIndicatorEmployeeState extends State<PageIndicatorEmployee> {
                     ),
                   ),
                   IconButton(
-                    onPressed: _currentPage < _pages.length - 1
+                    onPressed: _isNextButtonEnabled
                         ? () {
-                            _updateUserData(); // Update before going forward
-                            _controller.nextPage(
-                              duration: const Duration(milliseconds: 500),
-                              curve: Curves.ease,
-                            );
+                            _updateUserData();
+                            if (_currentPage < _pages.length - 1) {
+                              _controller.nextPage(
+                                duration: const Duration(milliseconds: 500),
+                                curve: Curves.ease,
+                              );
+                            } else {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder:
+                                      (context, animation1, animation2) {
+                                    return SlideTransition(
+                                      position: Tween<Offset>(
+                                        begin: const Offset(1, 0),
+                                        end: Offset.zero,
+                                      ).animate(CurveTween(
+                                              curve: Curves
+                                                  .fastEaseInToSlowEaseOut)
+                                          .animate(animation1)),
+                                      child: const Material(
+                                        elevation: 5,
+                                        child: SignUpEmployeeScreen7(),
+                                      ),
+                                    );
+                                  },
+                                  transitionDuration:
+                                      const Duration(milliseconds: 400),
+                                ),
+                              );
+                            }
                           }
-                        : () {
-                            Navigator.push(
-                              context,
-                              PageRouteBuilder(
-                                pageBuilder: (BuildContext context,
-                                    Animation<double> animation1,
-                                    Animation<double> animation2) {
-                                  return SlideTransition(
-                                    position: Tween<Offset>(
-                                      begin: const Offset(1, 0),
-                                      end: Offset.zero,
-                                    ).animate(CurveTween(
-                                            curve:
-                                                Curves.fastEaseInToSlowEaseOut)
-                                        .animate(animation1)),
-                                    child: const Material(
-                                      elevation: 5,
-                                      child: SignUpEmployeeScreen7(),
-                                    ),
-                                  );
-                                },
-                                transitionDuration:
-                                    const Duration(milliseconds: 400),
-                              ),
-                            );
-                          },
-                    icon: const Icon(
+                        : null, // Disable button
+                    icon: Icon(
                       Icons.arrow_forward_ios_rounded,
-                      color: blackColor,
+                      color: _isNextButtonEnabled ? blackColor : Colors.grey,
                     ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: PageView(
+              child: PageView.builder(
                 controller: _controller,
+                physics: _isNextButtonEnabled
+                    ? const AlwaysScrollableScrollPhysics()
+                    : const NeverScrollableScrollPhysics(),
                 onPageChanged: (index) {
-                  setState(() {
-                    _currentPage = index;
-                  });
-                  _updateUserData(); // Update user data when page changes
+                  setState(() => _currentPage = index);
+                  _validateCurrentPage(); // Validate when page changes
                 },
-                children: _pages,
+                itemCount: _pages.length,
+                itemBuilder: (context, index) {
+                  return _pages[index];
+                },
               ),
             ),
           ],
