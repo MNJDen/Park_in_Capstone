d973ca704a7821aa120a5c5f605b7f8935926510
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-31 20:02:25 +0800

    Screens: Update Alingal B 4W Student

--- lib/screens/parking areas/four wheels student/alingal_B_4W.dart
+++ lib/screens/parking areas/four wheels student/alingal_B_4W.dart
@@ -3,15 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:navbar_router/navbar_router.dart';
 import 'package:park_in/components/theme/color_scheme.dart';
+import 'package:park_in/components/ui/switch_btn.dart';
 
-class AlingalB4W extends StatefulWidget {
-  const AlingalB4W({super.key});
-
-  @override
-  _AlingalB4WState createState() => _AlingalB4WState();
+void showAlingalB4WBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: bgColor,
+    showDragHandle: true,
+    useSafeArea: true,
+    builder: (context) {
+      return PopScope(
+          canPop: true,
+          onPopInvokedWithResult: (didPop, result) async {
+            if (didPop) {
+              NavbarNotifier.hideBottomNavBar = false;
+              return;
+            }
+          },
+          child: const AlingalB4WBottomSheet());
+    },
+  );
 }
 
-class _AlingalB4WState extends State<AlingalB4W> {
+class AlingalB4WBottomSheet extends StatefulWidget {
+  const AlingalB4WBottomSheet({super.key});
+
+  @override
+  _AlingalB4WBottomSheetState createState() => _AlingalB4WBottomSheetState();
+}
+
+class _AlingalB4WBottomSheetState extends State<AlingalB4WBottomSheet> {
   final DatabaseReference _databaseReference =
       FirebaseDatabase.instance.ref().child('parkingAreas');
 
@@ -42,15 +64,15 @@ class _AlingalB4WState extends State<AlingalB4W> {
 
   Color _getStatusColor() {
     if (_alingalBAvailableSpace == 0) {
-      return parkingRedColor; // Red when full.
+      return parkingRedColor; 
     } else if (_alingalBAvailableSpace == _maxSpace) {
-      return parkingGreenColor; // Green when completely empty.
+      return const Color.fromARGB(255, 17, 194, 1); 
     } else if (_alingalBAvailableSpace < 13 && _alingalBAvailableSpace > 7) {
-      return parkingGreenColor;
+      return const Color.fromARGB(255, 17, 194, 1);
     } else if (_alingalBAvailableSpace <= 7 && _alingalBAvailableSpace > 5) {
-      return parkingYellowColor; // Yellow when mid-way full.
+      return parkingYellowColor; 
     } else if (_alingalBAvailableSpace <= 5) {
-      return parkingOrangeColor; // Orange when almost full.
+      return parkingOrangeColor; 
     } else {
       return parkingYellowColor;
     }
@@ -58,15 +80,15 @@ class _AlingalB4WState extends State<AlingalB4W> {
 
   String _getStatusText() {
     if (_alingalBAvailableSpace == 0) {
-      return "Full"; // Red status.
+      return "Full"; 
     } else if (_alingalBAvailableSpace == _maxSpace) {
-      return "Plenty of Space"; // Green status.
+      return "Plenty of Space"; 
     } else if (_alingalBAvailableSpace < 13 && _alingalBAvailableSpace > 7) {
-      return "Plenty of Space"; // Green status.
+      return "Plenty of Space"; 
     } else if (_alingalBAvailableSpace <= 7 && _alingalBAvailableSpace > 5) {
-      return "Half Full"; // Yellow status.
+      return "Half Full"; 
     } else if (_alingalBAvailableSpace <= 5) {
-      return "Almost Full"; // Orange status.
+      return "Almost Full"; 
     } else {
       return "Filling";
     }
@@ -74,160 +96,103 @@ class _AlingalB4WState extends State<AlingalB4W> {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) async {
-        if (didPop) {
-          NavbarNotifier.hideBottomNavBar = false;
-          return;
-        }
-      },
-      child: Scaffold(
-        backgroundColor: bgColor,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.pop(
-                                context,
-                              );
-                              NavbarNotifier.hideBottomNavBar = false;
-                            },
-                            child: const Icon(
-                              Icons.arrow_back_ios_new_rounded,
-                              color: blackColor,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          "Alingal B",
-                          style: TextStyle(
-                            fontSize: 20.r,
-                            color: blueColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Current Capacity:",
-                              style: TextStyle(
-                                fontSize: 12.sp,
-                                color: blackColor,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 2.h,
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Status:",
-                                  style: TextStyle(
-                                    fontSize: 12.sp,
-                                    color: blackColor,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 8.w,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8.w,
-                                    vertical: 2.h,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: _getStatusColor().withOpacity(0.07),
-                                    borderRadius: BorderRadius.circular(100),
-                                  ),
-                                  child: Text(
-                                    _getStatusText(),
-                                    style: TextStyle(
-                                      color: _getStatusColor(),
-                                      fontSize: 12.sp,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const Spacer(),
-                        Text(
-                          "$_alingalBAvailableSpace",
-                          style: TextStyle(
-                            fontSize: 52.sp,
-                            color: blackColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 40.h,
-                    ),
-                    Image.asset(
-                      "assets/building_images/AlingalB-4W-S.png",
-                      width: 352.w,
-                      height: 233.h,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 40.h),
-                  child: Row(
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              "Alingal B",
+              style: TextStyle(
+                fontSize: 20.r,
+                color: blueColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SizedBox(height: 20.h),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Current Capacity:",
+                    style: TextStyle(fontSize: 12.sp, color: blackColor),
+                  ),
+                  SizedBox(height: 2.h),
+                  Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Icon(
-                        Icons.info_rounded,
-                        color: blackColor,
+                      Text(
+                        "Status:",
+                        style: TextStyle(fontSize: 12.sp, color: blackColor),
                       ),
-                      SizedBox(
-                        width: 12.w,
-                      ),
-                      Flexible(
+                      SizedBox(width: 8.w),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8.w,
+                          vertical: 2.h,
+                        ),
+                        decoration: BoxDecoration(
+                          color: _getStatusColor().withOpacity(0.07),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
                         child: Text(
-                          "Always be mindful of the space between vehicles.",
-                          softWrap: true,
+                          _getStatusText(),
                           style: TextStyle(
-                            color: blackColor,
+                            color: _getStatusColor(),
                             fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
                       ),
                     ],
                   ),
+                ],
+              ),
+              const Spacer(),
+              Text(
+                "$_alingalBAvailableSpace",
+                style: TextStyle(
+                  fontSize: 52.sp,
+                  color: blackColor,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
+          SizedBox(height: 30.h),
+          Image.asset(
+            "assets/building_images/AlingalB-4W-S.png",
+            width: 352.w,
+            height: 233.h,
+          ),
+          SizedBox(height: 30.h),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(Icons.info_rounded, color: blackColor),
+              SizedBox(width: 12.w),
+              Flexible(
+                child: Text(
+                  "Always be mindful of the space between vehicles.",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: blackColor,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.h),
+          const PRKSwitchBtn(),
+          SizedBox(height: 40.h),
+        ],
       ),
     );
   }
