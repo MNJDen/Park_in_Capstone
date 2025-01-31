ed86c4c5d82f8ad3d4945d8d4b27191f35c8a536
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-31 20:00:17 +0800

    Components: Update Library 4W Student

--- lib/components/four wheels student/library.dart
+++ lib/components/four wheels student/library.dart
@@ -70,28 +70,7 @@ class _PRKCCB4WStundentState extends State<PRKLibrary4WStundent>
             const Duration(milliseconds: 350),
             () {
               _animationController.reverse();
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (BuildContext context,
-                      Animation<double> animation1,
-                      Animation<double> animation2) {
-                    return SlideTransition(
-                      position: Tween<Offset>(
-                        begin: const Offset(1, 0),
-                        end: Offset.zero,
-                      ).animate(
-                          CurveTween(curve: Curves.fastEaseInToSlowEaseOut)
-                              .animate(animation1)),
-                      child: const Material(
-                        elevation: 5,
-                        child: Library4w(),
-                      ),
-                    );
-                  },
-                  transitionDuration: const Duration(milliseconds: 400),
-                ),
-              );
+              showLibrary4WBottomSheet(context);
               NavbarNotifier.hideBottomNavBar = true;
             },
           );
