9f6accc619d927aace90b27fe0699b082ea3461c
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-31 20:00:04 +0800

    Components: Update Coko Cafe 4W Student

--- lib/components/four wheels student/coko_cafe.dart
+++ lib/components/four wheels student/coko_cafe.dart
@@ -70,28 +70,7 @@ class _PRKCoko4WStundentState extends State<PRKCoko4WStundent>
             const Duration(milliseconds: 350),
             () {
               _animationController.reverse();
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (BuildContext context,
-                      Animation<double> animation1,
-                      Animation<double> animation2) {
-                    return SlideTransition(
-                      position: Tween<Offset>(
-                        begin: const Offset(1, 0),
-                        end: Offset.zero,
-                      ).animate(
-                          CurveTween(curve: Curves.fastEaseInToSlowEaseOut)
-                              .animate(animation1)),
-                      child: const Material(
-                        elevation: 5,
-                        child: Coko4w(),
-                      ),
-                    );
-                  },
-                  transitionDuration: const Duration(milliseconds: 400),
-                ),
-              );
+              showCoko4WBottomSheet(context);
               NavbarNotifier.hideBottomNavBar = true;
             },
           );
