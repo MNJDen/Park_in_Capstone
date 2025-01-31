dee4518db345185973a513c756724da591f652ad
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-31 20:02:51 +0800

    Screens: Update CC 4W Student

--- lib/screens/parking areas/four wheels student/Covered_Court_4W.dart
+++ lib/screens/parking areas/four wheels student/Covered_Court_4W.dart
@@ -3,18 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:navbar_router/navbar_router.dart';
 import 'package:park_in/components/theme/color_scheme.dart';
+import 'package:park_in/components/ui/switch_btn.dart';
 
-class CoveredCourt4w extends StatefulWidget {
-  const CoveredCourt4w({super.key});
-
-  _CoveredCourt4w createState() => _CoveredCourt4w();
+void showCC4WBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: bgColor,
+    showDragHandle: true,
+    useSafeArea: true,
+    builder: (context) {
+      return PopScope(
+          canPop: true,
+          onPopInvokedWithResult: (didPop, result) async {
+            if (didPop) {
+              NavbarNotifier.hideBottomNavBar = false;
+              return;
+            }
+          },
+          child: const CoveredCourt4wBottomSheet());
+    },
+  );
 }
 
-class _CoveredCourt4w extends State<CoveredCourt4w> {
+class CoveredCourt4wBottomSheet extends StatefulWidget {
+  const CoveredCourt4wBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  _CoveredCourt4wBottomSheetState createState() =>
+      _CoveredCourt4wBottomSheetState();
+}
+
+class _CoveredCourt4wBottomSheetState extends State<CoveredCourt4wBottomSheet> {
   final DatabaseReference _databaseReference =
       FirebaseDatabase.instance.ref().child('parkingAreas');
 
   int _coveredCourtAvailableSpace = 0;
+
+  final int _maxSpace = 19;
 
   @override
   void initState() {
@@ -37,21 +63,19 @@ class _CoveredCourt4w extends State<CoveredCourt4w> {
     });
   }
 
-  final int _maxSpace = 19;
-
   Color _getStatusColor() {
     if (_coveredCourtAvailableSpace == 0) {
-      return parkingRedColor; // Red when full.
+      return parkingRedColor;
     } else if (_coveredCourtAvailableSpace == _maxSpace) {
-      return parkingGreenColor; // Green when completely empty.
+      return const Color.fromARGB(255, 17, 194, 1);
     } else if (_coveredCourtAvailableSpace < 19 &&
         _coveredCourtAvailableSpace > 9) {
-      return parkingGreenColor;
+      return const Color.fromARGB(255, 17, 194, 1);
     } else if (_coveredCourtAvailableSpace <= 9 &&
         _coveredCourtAvailableSpace > 5) {
-      return parkingYellowColor; // Yellow when mid-way full.
+      return parkingYellowColor;
     } else if (_coveredCourtAvailableSpace <= 5) {
-      return parkingOrangeColor; // Orange when almost full.
+      return parkingOrangeColor;
     } else {
       return parkingYellowColor;
     }
@@ -59,17 +83,17 @@ class _CoveredCourt4w extends State<CoveredCourt4w> {
 
   String _getStatusText() {
     if (_coveredCourtAvailableSpace == 0) {
-      return "Full"; // Red status.
+      return "Full";
     } else if (_coveredCourtAvailableSpace == _maxSpace) {
-      return "Plenty of Space"; // Green status.
+      return "Plenty of Space";
     } else if (_coveredCourtAvailableSpace < 19 &&
         _coveredCourtAvailableSpace > 9) {
-      return "Plenty of Space"; // Green status.
+      return "Plenty of Space";
     } else if (_coveredCourtAvailableSpace <= 9 &&
         _coveredCourtAvailableSpace > 5) {
-      return "Half Full"; // Yellow status.
+      return "Half Full";
     } else if (_coveredCourtAvailableSpace <= 5) {
-      return "Almost Full"; // Orange status.
+      return "Almost Full";
     } else {
       return "Filling";
     }
@@ -77,160 +101,103 @@ class _CoveredCourt4w extends State<CoveredCourt4w> {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) async {
-        if (didPop) {
-          NavbarNotifier.hideBottomNavBar = false;
-          return;
-        }
-      },
-      child: Scaffold(
-        backgroundColor: bgColor,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.pop(
-                                context,
-                              );
-                              NavbarNotifier.hideBottomNavBar = false;
-                            },
-                            child: const Icon(
-                              Icons.arrow_back_ios_new_rounded,
-                              color: blackColor,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          "Covered Court",
-                          style: TextStyle(
-                            fontSize: 20.r,
-                            color: blueColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Current Capacity:",
-                              style: TextStyle(
-                                fontSize: 12.sp,
-                                color: blackColor,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 2.h,
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Status:",
-                                  style: TextStyle(
-                                    fontSize: 12.sp,
-                                    color: blackColor,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 8.w,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8.w,
-                                    vertical: 2.h,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: _getStatusColor().withOpacity(0.07),
-                                    borderRadius: BorderRadius.circular(100),
-                                  ),
-                                  child: Text(
-                                    _getStatusText(),
-                                    style: TextStyle(
-                                      color: _getStatusColor(),
-                                      fontSize: 12.sp,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const Spacer(),
-                        Text(
-                          "$_coveredCourtAvailableSpace",
-                          style: TextStyle(
-                            fontSize: 52.sp,
-                            color: blackColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 40.h,
-                    ),
-                    Image.asset(
-                      "assets/building_images/CC-4W-S.png",
-                      width: 358.w,
-                      height: 242.h,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 40.h),
-                  child: Row(
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              "Covered Court",
+              style: TextStyle(
+                fontSize: 20.r,
+                color: blueColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SizedBox(height: 20.h),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Current Capacity:",
+                    style: TextStyle(fontSize: 12.sp, color: blackColor),
+                  ),
+                  SizedBox(height: 2.h),
+                  Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Icon(
-                        Icons.info_rounded,
-                        color: blackColor,
+                      Text(
+                        "Status:",
+                        style: TextStyle(fontSize: 12.sp, color: blackColor),
                       ),
-                      SizedBox(
-                        width: 12.w,
-                      ),
-                      Flexible(
+                      SizedBox(width: 8.w),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8.w,
+                          vertical: 2.h,
+                        ),
+                        decoration: BoxDecoration(
+                          color: _getStatusColor().withOpacity(0.07),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
                         child: Text(
-                          "Always be mindful of the space between vehicles.",
-                          softWrap: true,
+                          _getStatusText(),
                           style: TextStyle(
-                            color: blackColor,
+                            color: _getStatusColor(),
                             fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
                       ),
                     ],
                   ),
+                ],
+              ),
+              const Spacer(),
+              Text(
+                "$_coveredCourtAvailableSpace",
+                style: TextStyle(
+                  fontSize: 52.sp,
+                  color: blackColor,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
+          SizedBox(height: 30.h),
+          Image.asset(
+            "assets/building_images/CC-4W-S.png",
+            width: 358.w,
+            height: 242.h,
+          ),
+          SizedBox(height: 30.h),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(Icons.info_rounded, color: blackColor),
+              SizedBox(width: 12.w),
+              Flexible(
+                child: Text(
+                  "Always be mindful of the space between vehicles.",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: blackColor,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.h),
+          const PRKSwitchBtn(),
+          SizedBox(height: 40.h),
+        ],
       ),
     );
   }
