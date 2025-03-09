9261f2cb2ea19876864a960eeb7f9d0c1f8c92e2
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2025-03-09 15:54:54 +0800

    Providers: Update User Data Provider

--- lib/providers/user_data_provider.dart
+++ lib/providers/user_data_provider.dart
@@ -4,11 +4,15 @@ import 'package:flutter/material.dart';
 
 class UserData {
   String? name;
+  String? email;
   String? userNumber;
   String? phoneNumber;
   String? password;
   String? imageUrl;
+  String? imageUrl1;
+  File? imageFile1;
   File? imageFile;
+  String? imagePath;
   String? usertype;
   String? department;
   List<String> stickerNumber;
@@ -16,11 +20,15 @@ class UserData {
 
   UserData({
     this.name,
+    this.email,
     this.userNumber,
     this.phoneNumber,
     this.password,
     this.imageUrl,
+    this.imageUrl1,
+    this.imagePath,
     this.imageFile,
+    this.imageFile1,
     this.department,
     List<String>? stickerNumber,
     List<String>? plateNumber,
@@ -30,11 +38,15 @@ class UserData {
 
   UserData copyWith({
     String? name,
+    String? email,
     String? userNumber,
     String? phoneNumber,
     String? password,
     String? imageUrl,
+    String? imageUrl1,
+    String? imagePath,
     File? imageFile,
+    File? imageFile1,
     String? usertype,
     String? department,
     List<String>? stickerNumber,
@@ -42,11 +54,15 @@ class UserData {
   }) {
     return UserData(
       name: name ?? this.name,
+      email: email ?? this.email,
       userNumber: userNumber ?? this.userNumber,
       phoneNumber: phoneNumber ?? this.phoneNumber,
       password: password ?? this.password,
+      imagePath: imagePath ?? this.imagePath,
       imageUrl: imageUrl ?? this.imageUrl,
+      imageUrl1: imageUrl1 ?? this.imageUrl1,
       imageFile: imageFile != null ? imageFile : null,
+      imageFile1: imageFile1 != null ? imageFile1 : null,
       usertype: usertype ?? this.usertype,
       department: department ?? this.department,
       stickerNumber: stickerNumber ?? List<String>.from(this.stickerNumber),
@@ -63,11 +79,15 @@ class UserDataProvider extends ChangeNotifier {
   /// Update the user data with the provided values.
   void updateUserData({
     String? name,
+    String? email,
     String? userNumber,
     String? phoneNumber,
     String? password,
     String? imageUrl,
+    String? imageUrl1,
     File? imageFile,
+    File? imageFile1,
+    String? imagePath,
     String? usertype,
     String? department,
     List<String>? stickerNumber,
@@ -75,11 +95,15 @@ class UserDataProvider extends ChangeNotifier {
   }) {
     _userData = _userData.copyWith(
       name: name,
+      email: email,
       userNumber: userNumber,
       phoneNumber: phoneNumber,
       password: password,
       imageUrl: imageUrl,
+      imageUrl1: imageUrl1,
       imageFile: imageFile,
+      imageFile1: imageFile1,
+      imagePath: imagePath,
       usertype: usertype,
       department: department,
       stickerNumber: stickerNumber,
