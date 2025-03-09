8b6607778c13cc641970a7d31f2e9b899ea8ecff
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-03-09 21:33:20 +0800

    Services: Update Auth Gate

--- lib/services/auth/Auth_Gate.dart
+++ lib/services/auth/Auth_Gate.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:park_in/components/bottom%20nav%20bar/bottom_nav_bar_employee.dart';
 import 'package:park_in/components/bottom%20nav%20bar/bottom_nav_bar_student.dart';
-import 'package:park_in/screens/home%20admin/home_admin.dart';
+import 'package:park_in/screens/misc/verification.dart';
 import 'package:park_in/screens/sign%20in/sign_in_student_employee.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,6 +14,7 @@ class AuthGate extends StatefulWidget {
 
 class _AuthGateState extends State<AuthGate> {
   String? _userType;
+  String? _verificationStatus;
 
   @override
   void initState() {
@@ -26,18 +28,45 @@ class _AuthGateState extends State<AuthGate> {
 
     if (isLoggedIn) {
       final userType = prefs.getString('userType');
-      setState(() {
-        _userType = userType;
-      });
+      final userId = prefs.getString('userId');
+
+      if (userId != null) {
+        final verificationStatus =
+            await _getVerificationStatus(userId, userType);
+        print("User Verification Status: $verificationStatus");
+
+        setState(() {
+          _userType = userType;
+          _verificationStatus = verificationStatus;
+        });
+      }
     }
+  }
+
+  Future<String?> _getVerificationStatus(
+      String userId, String? userType) async {
+    try {
+      if (userType == 'Student' || userType == 'Employee') {
+        final doc = await FirebaseFirestore.instance
+            .collection('User')
+            .doc(userId)
+            .get();
+        if (doc.exists) {
+          return doc['status'];
+        }
+      }
+    } catch (e) {
+      print("Error fetching verification status: $e");
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_userType == null) {
-      return const SignInScreen(); // Or another initial screen
-    } else if (_userType == 'Admin') {
-      return const HomeAdminScreen1();
+    if (_userType == null || _verificationStatus == null) {
+      return const SignInScreen();
+    } else if (_verificationStatus == 'non-verified') {
+      return const NonVerified();
     } else if (_userType == 'Student') {
       return const BottomNavBarStudent();
     } else if (_userType == 'Employee') {
